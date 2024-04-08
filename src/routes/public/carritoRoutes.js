3ba0a291fbc64d544a05b03dc8233fe739d8d9b3
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-04-08 01:25:35 -0600

    update

--- src/routes/public/carritoRoutes.js
+++ src/routes/public/carritoRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const carritoController = require('../../controllers/public/carritoController');
 
 // Rutas para el carrito
-router.get('/:customerId', carritoController.verifyToken, carritoController.getCarrito);
-router.post('/',carritoController.verifyToken, carritoController.addToCarrito);
-router.put('/:customerId/:productoId',carritoController.verifyToken, carritoController.updateCarrito);
-router.delete('/',carritoController.verifyToken, carritoController.deleteCarrito);
-router.delete('/clear/:customerId',carritoController.verifyToken, carritoController.clearCarrito);
+router.get('/:customerId',  carritoController.getCarrito);
+router.post('/', carritoController.addToCarrito);
+router.put('/:customerId/:productoId', carritoController.updateCarrito);
+router.delete('/', carritoController.deleteCarrito);
+router.delete('/clear/:customerId', carritoController.clearCarrito);
 
 module.exports = router;
