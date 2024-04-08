1318499932ed4ce5f5a5d2c3dc90aff2cd19e53b
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-04-08 01:16:35 -0600

    update

--- src/controllers/public/carritoController.js
+++ src/controllers/public/carritoController.js
@@ -76,7 +76,7 @@ const carritoController = {
     }
   },
   verifyToken: (req, res, next) => {
-    const token = req.headers.authorization;
+    const token = req.headers['authorization'];
     if (!token) {
       return res.status(401).json({ error: "Token no proporcionado" });
     }
