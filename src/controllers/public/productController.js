8da19dc273982d9ff81d8cab3dfe9ec6a37af4bf
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-04-14 23:45:59 -0600

    update sequalize

--- src/controllers/public/productController.js
+++ src/controllers/public/productController.js
@@ -1,5 +1,6 @@
 // /src/controllers/productController.js
 // Importa tus modelos aquí
+const { sequelize } = require('../../../src/config/database');
 const Producto = require("../../../models/productsModel");
 const Categoria = require("../../../models/categoriaModel");
 const DetalleVenta = require("../../../models/detalleVentaModel")
