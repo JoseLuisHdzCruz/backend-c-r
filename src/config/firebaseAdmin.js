f4cccb930b9c4f13aa5788c73bb8dc43b74e8853
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-11-03 17:11:50 -0600

    update key firebase

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,6 +1,6 @@
 // src/config/firebaseAdmin.js
 const admin = require("firebase-admin");
-const serviceAccount = require("../../chucheriasapp-firebase-adminsdk-2ch2b-f7e2b4d80d.json")
+const serviceAccount = require("../../chucheriasapp-firebase-adminsdk-2ch2b-043957698c.json")
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
