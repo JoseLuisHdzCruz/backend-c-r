76b6bba66e99e82ef80b133c05a3f108a1373efe
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-11-03 17:00:59 -0600

    update key id

--- src/jobs/cartNotifications.js
+++ src/jobs/cartNotifications.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const { notifyAbandonedCart } = require("../controllers/public/carritoController"); // Asegúrate de que el path sea correcto
 
 // Ejecuta notifyAbandonedCart todos los días a la medianoche
-cron.schedule("*/2 * * * *", notifyAbandonedCart, {
+cron.schedule("*/1 * * * *", notifyAbandonedCart, {
     scheduled: true,
     timezone: "America/Mexico_City" // Ajusta a tu zona horaria si es necesario
   });
