efaf9e208b8f4d3d1aee1f298889cebac036cb98
Itzel Doroteo Martinez <20211311@uthh.edu.mx>
2024-11-07 14:17:52 -0600

    update_usercontroller

--- src/jobs/cartNotifications.js
+++ src/jobs/cartNotifications.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const { notifyAbandonedCart } = require("../controllers/public/carritoController"); // Asegúrate de que el path sea correcto
 
 // Ejecuta notifyAbandonedCart todos los días a la medianoche
-cron.schedule("*/1 * * * *", notifyAbandonedCart, {
+cron.schedule("*/59 * * * *", notifyAbandonedCart, {
     scheduled: true,
     timezone: "America/Mexico_City" // Ajusta a tu zona horaria si es necesario
   });
